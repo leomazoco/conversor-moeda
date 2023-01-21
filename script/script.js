008e93e58d64e9f9841039364724f07a2ee12aeb
Leonardo Mazoco <115808428+leomazoco@users.noreply.github.com>
2023-01-21 12:54:45 -0300

    fixed

--- script/script.js
+++ script/script.js
@@ -1,7 +1,13 @@
-var valorNormal = document.getElementsByClassName('digitado').value;
-
-var valorConverter = Number(valorNormal.value)
+var valorNormal = document.getElementById('valorInteiro');
+var valorDolar = 5.21;
+var valorEuro = 5.66;
+var valorLibra = 6.45;
 
 function converter() {
-    console.log(valorNormal)
+
+    var valorConverter = Number(valorNormal.value);
+
+    let valorConvertido = (valorConverter * valorDolar).toFixed(2)
+
+    console.log(valorConvertido)
 }
